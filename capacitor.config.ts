f9515153bc39882f8bc5a9f287c439e9dcbc73ad
Carlos Daniel Medina Sahagun <carlos.medina1311@alumnos.udg.mx>
2021-11-30 07:32:17 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'agenda_digital',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
